0a4fa5a4372050c50f09603f44857d94a89779aa
yeongseon <yeongseon.choe@gmail.com>
2016-08-08 13:51:08 +0900

    Add flask_googlemaps

--- routes.py
+++ routes.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map
 
 app = Flask(__name__)
 Bootstrap(app)
+GoogleMaps(app)
 
 @app.route('/')
 def home():
@@ -22,7 +25,15 @@ def menu3():
 
 @app.route('/navigation')
 def navigation():
-  return render_template('navigation.html')
+  mymap = Map(
+          identifier="view_side",
+          lat=34.749064,
+          lng=127.655273,
+          zoom=15,
+          style="height:300px;width:device-width;margin:0;",
+          markers=[(34.749063, 127.655273)]
+          )
+  return render_template('navigation.html', mymap=mymap)
 
 if __name__ == '__main__':
   app.run(debug=True)
